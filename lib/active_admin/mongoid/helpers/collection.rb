784bf177d8f4165a1eb78388dcd52eff1caf12e7
Eugene Sobolev <sobolevea@ukr.net>
2015-01-12 15:44:42 +0200

    Update collection.rb

--- lib/active_admin/mongoid/helpers/collection.rb
+++ lib/active_admin/mongoid/helpers/collection.rb
@@ -2,7 +2,7 @@ module ActiveAdmin
   module Helpers
     module Collection
       def collection_size(collection=collection)
-        collection.count(true)
+        collection.count(true) rescue 0
       end
     end
   end
